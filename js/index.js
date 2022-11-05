8ce1fc1a0caa2619cb03ab954e7e6eba420ce081
Estefania Egea <estefania.egcal@gmail.com>
2022-11-05 01:16:43 +0100

    ejercicio 1 y dos hechos

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
 // Iteration 1: Names and Input
+
+console.log("I'm ready!");
+
+let hacker1 = "Estefania";
+console.log("The driver's name is ${hacker1");
+
+let hacker2 = "Carlos";
+console.log("The navigator's name is ${hacker2");
 //
 // Iteration 2: Conditionals
 
